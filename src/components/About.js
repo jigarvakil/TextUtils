6a6ecb114ee922704a4f5cb735fa76da0d3092ca
jigarvakil <>
2021-11-29 11:35:09 +0530

    init commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <>
+      <div className="container">
+        <h1>Hello I am jigar vakil.</h1>
+      </div>
+    </>
+  );
+}
